53332109c2a5131d0fa8b408f2a99fd0c5eddce3
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2025-02-07 21:51:54 +1300

    refactor: update logo size

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,13 +5,13 @@ import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 export const Header = () => {
   return (
-    <header className="h-[60px] flex items-center justify-between px-[16px] py-[4px] sm:px-14 sticky top-0 bg-clip-padding backdrop-filter backdrop-blur-md border-b">
+    <header className="h-[60px] flex items-center justify-between px-[16px] py-[4px] sticky top-0 bg-clip-padding backdrop-filter backdrop-blur-md border-b">
       <Image
         src={
           "https://liveheats.com/static/media/LockupHorizontalBrandRed.87dd46f84eda84658a198c99d5cf1e3f.svg"
         }
-        width={140}
-        height={140}
+        width={136}
+        height={136}
         alt={"Logo"}
       />
 
