52b95c5edbc3c6f0b218a85415329db9c3a155d1
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2025-02-08 00:09:25 +1300

    refactor: create RaceCreateCard skeleton

---
+++ src/features/RaceGallery/Components/RaceCreateCard/RaceCreateCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Plus } from "lucide-react";
+
+export const RaceCreateCard = () => {
+  return (
+    <div
+      className={
+        "cursor-pointer w-full bg-white p-4 rounded-[16px] border-dashed border-2 border-gray-300 flex justify-center items-center text-muted-foreground " +
+        "hover:bg-gradient-to-r from-red-500 to-orange-500 hover:border-none hover:text-white"
+      }
+    >
+      <Plus className={"mr-1"} />
+      <span className={"font-bold"}>Start a new race!</span>
+    </div>
+  );
+};
