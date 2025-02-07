a8d0e66f2e74f8c9bb8fe5fa06da9a5c6a4600d8
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2025-02-07 23:37:57 +1300

    refactor: implement gallery skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import RaceGallery from "@/features/RaceGallery/RaceGallery";
+
 const Home = () => {
-  return <div className={"text-3xl font-semibold"}>Race Manager</div>;
+  return (
+    <>
+      <div className={"text-3xl font-semibold"}>Race Manager</div>
+      <RaceGallery />
+    </>
+  );
 };
 
 export default Home;
