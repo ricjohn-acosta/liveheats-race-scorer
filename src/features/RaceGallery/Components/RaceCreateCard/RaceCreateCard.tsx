e032fdb772da6bf9a2b8b73af8cbce60d199efc4
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2025-02-08 01:04:37 +1300

    refactor: set fixed heights to cards

--- src/features/RaceGallery/Components/RaceCreateCard/RaceCreateCard.tsx
+++ src/features/RaceGallery/Components/RaceCreateCard/RaceCreateCard.tsx
@@ -5,7 +5,7 @@ export const RaceCreateCard = () => {
   return (
     <div
       className={
-        "cursor-pointer w-full bg-white p-4 rounded-[16px] border-dashed border-2 border-gray-300 flex justify-center items-center text-muted-foreground " +
+        "cursor-pointer w-full bg-white h-24 p-4 rounded-[16px] border-dashed border-2 border-gray-300 flex justify-center items-center text-muted-foreground " +
         "hover:bg-gradient-to-r from-red-500 to-orange-500 hover:border-none hover:text-white"
       }
     >
