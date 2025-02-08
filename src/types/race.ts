e916e61e68038a61489606c0c8b94aca00503a50
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2025-02-09 12:49:58 +1300

    refactor: create types for race

---
+++ src/types/race.ts
@@ -0,0 +1,12 @@
+export type RaceParticipant = {
+  lane: number;
+  participantName: string;
+};
+
+export type Race = {
+  raceId: string;
+  raceParticipants: RaceParticipant[];
+  raceName: string;
+  status: string;
+  createdAt: Date;
+};
