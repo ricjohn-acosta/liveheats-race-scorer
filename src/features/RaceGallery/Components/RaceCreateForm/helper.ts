4f484bbb7c8c5fb87ce902730e2a9e86af50ae56
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2025-02-08 22:56:29 +1300

    refactor: update form schema

--- src/features/RaceGallery/Components/RaceCreateForm/helper.ts
+++ src/features/RaceGallery/Components/RaceCreateForm/helper.ts
@@ -1,5 +1,15 @@
 import { z } from "zod";
 
-export const RaceCreateFormSchema = z.object({});
+export const RaceCreateFormSchema = z.object({
+  raceName: z.string(),
+  raceParticipants: z
+    .array(
+      z.object({
+        lane: z.number(),
+        participantName: z.string(),
+      }),
+    )
+    .min(2, "At least two participants are required."),
+});
 
 export type RaceCreateForm = z.infer<typeof RaceCreateFormSchema>;
