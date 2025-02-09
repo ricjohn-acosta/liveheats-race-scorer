eb264073b664c60ee088be541ea3f91a7aaf9e5d
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2025-02-09 23:29:29 +1300

    refactor: add finish race button

--- src/features/RaceDetail/Components/RaceAthletesTable/RaceAthletesTable.tsx
+++ src/features/RaceDetail/Components/RaceAthletesTable/RaceAthletesTable.tsx
@@ -16,6 +16,7 @@ import {
 import { FC } from "react";
 import { Race } from "@/types/race";
 import { useRaceAthletesTable } from "@/features/RaceDetail/Components/RaceAthletesTable/useRaceAthletesTable";
+import { Button } from "@/components/ui/button";
 
 interface RaceAthletesTableProps {
   race: Race;
@@ -23,51 +24,69 @@ interface RaceAthletesTableProps {
 
 export const RaceAthletesTable: FC<RaceAthletesTableProps> = ({ race }) => {
   const {
+    data: { results },
     operations: { handlePlaceSelect },
   } = useRaceAthletesTable();
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Lane</TableHead>
-          <TableHead>Athlete</TableHead>
-          <TableHead>Place</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {race.raceParticipants.map((participant, i) => {
-          return (
-            <TableRow key={i}>
-              <TableCell className={"font-archivo text-2xl font-semibold pl-3"}>
-                {participant.lane}
-              </TableCell>
-              <TableCell>{participant.participantName}</TableCell>
-              <TableCell className={"w-1/3"}>
-                <Select
-                  onValueChange={(value) =>
-                    handlePlaceSelect(participant.lane, Number(value))
-                  }
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Lane</TableHead>
+            <TableHead>Athlete</TableHead>
+            <TableHead>Place</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {race.raceParticipants.map((participant, i) => {
+            return (
+              <TableRow key={i}>
+                <TableCell
+                  className={"font-archivo text-2xl font-semibold pl-3"}
                 >
-                  <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {race.raceParticipants.map((_, index) => {
-                      const place = index + 1;
-                      return (
-                        <SelectItem key={index} value={place.toString()}>
-                          {place}
-                        </SelectItem>
-                      );
-                    })}
-                  </SelectContent>
-                </Select>
-              </TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+                  {participant.lane}
+                </TableCell>
+                <TableCell>{participant.participantName}</TableCell>
+                <TableCell className={"w-1/3"}>
+                  <Select
+                    onValueChange={(value) =>
+                      handlePlaceSelect(participant.lane, Number(value))
+                    }
+                  >
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {race.raceParticipants.map((_, index) => {
+                        const place = index + 1;
+                        return (
+                          <SelectItem key={index} value={place.toString()}>
+                            {place}
+                          </SelectItem>
+                        );
+                      })}
+                    </SelectContent>
+                  </Select>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+
+      <div className={"w-full flex justify-end"}>
+        {race.status === "live" && (
+          <Button
+            disabled={results.length !== race.raceParticipants.length}
+            className={
+              "mt-2 hover:bg-green-400 bg-green-500 font-bold font-archivo"
+            }
+          >
+            Finish race!
+          </Button>
+        )}
+      </div>
+    </>
   );
 };
