3f7287b258aa9bfff87f64e59a8c8fe9cbee387d
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2025-02-10 11:11:36 +1300

    refactor: specify color for live badge

--- src/components/StatusBadge/StatusBadge.tsx
+++ src/components/StatusBadge/StatusBadge.tsx
@@ -11,9 +11,10 @@ export const StatusBadge: FC<StatusBadgeProps> = ({ className, children }) => {
   return (
     <Badge
       className={cn(
-        "rounded-[4px] uppercase",
+        "rounded-[4px] uppercase font-bold",
         children === "completed" &&
           "bg-[#B3EBFF] hover:bg-[#B3EBFF] text-[#003EB3]",
+        children === "live" && "bg-[#E2FF43] hover:bg-[#E2FF43] text-[#303800]",
         className,
       )}
     >
