56c5c05f3ee82976fab32b9827aee8eb85e630ba
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2025-02-10 12:30:19 +1300

    test: create test for RaceCard.tsx

--- src/features/RaceGallery/Components/RaceCard/__tests__/RaceCard.test.tsx
+++ src/features/RaceGallery/Components/RaceCard/__tests__/RaceCard.test.tsx
@@ -7,22 +7,40 @@ jest.mock("lucide-react", () => ({
 }));
 
 describe("RaceCard", () => {
-  it("renders a race name", () => {
-    render(
-      <RaceCard
-        raceData={{
-          raceId: "TEST123",
-          raceName: "Usain Bolt vs Barry Allen",
-          raceParticipants: [
-            { lane: 1, participantName: "Usain Bolt", place: 0 },
-            { lane: 2, participantName: "Barry Allen", place: 0 },
-          ],
-          status: "live",
-          createdAt: new Date().toDateString(),
-        }}
-      />,
-    );
+  const props = {
+    raceId: "TEST123",
+    raceName: "Usain Bolt vs Barry Allen",
+    raceParticipants: [
+      { lane: 1, participantName: "Usain Bolt", place: 0 },
+      { lane: 2, participantName: "Barry Allen", place: 0 },
+    ],
+    status: "live",
+    createdAt: new Date().toDateString(),
+  };
 
+  it("renders a race name", () => {
+    render(<RaceCard raceData={props} />);
     expect(screen.getByText("Usain Bolt vs Barry Allen")).toBeInTheDocument();
+  });
+
+  it("renders number of participants correctly", () => {
+    render(<RaceCard raceData={props} />);
+    expect(screen.getByText("2")).toBeInTheDocument();
+  });
+
+  it("renders race status correctly", () => {
+    render(<RaceCard raceData={props} />);
+    expect(screen.getByText("live")).toBeInTheDocument();
+  });
+
+  it("renders a formatted date correctly", () => {
+    render(<RaceCard raceData={props} />);
+    const currentFormattedMonth = new Date()
+      .toLocaleString("en-US", { month: "short" })
+      .toUpperCase();
+    const currentDate = new Date().getDate().toString();
+
+    expect(screen.getByText(currentFormattedMonth)).toBeInTheDocument();
+    expect(screen.getByText(currentDate)).toBeInTheDocument();
   });
 });
