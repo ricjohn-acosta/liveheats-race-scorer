4aeb1009e299cf3a69304fd82288c428c0d0d4dc
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2025-02-09 13:05:06 +1300

    refactor: update hook so that it initializes localStorage with seeded data

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const useLocalStorage = <T,>(key: string, initialValue: T) => {
-  const storedValue = localStorage.getItem(key);
-  const parsedValue = storedValue ? JSON.parse(storedValue) : initialValue;
+  const [storedData, setStoredData] = useState<T>(() => {
+    try {
+      const storedValue = localStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : initialValue;
+    } catch (error) {
+      console.error("Error parsing localStorage:", error);
+      return initialValue;
+    }
+  });
 
-  const [storedData, setStoredData] = useState<T>(parsedValue);
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(storedData));
+  }, [key, storedData]);
 
   const setValue = (value: T) => {
     setStoredData(value);
