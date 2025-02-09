61b3dcbe2818bd21cbc1c3653102408eed024f7b
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2025-02-09 14:30:45 +1300

    refactor: seed data initially

--- src/features/RaceGallery/useRaceGallery.tsx
+++ src/features/RaceGallery/useRaceGallery.tsx
@@ -6,6 +6,25 @@ export const useRaceGallery = () => {
 
   useEffect(() => {
     const storedData = localStorage.getItem("liveHeatsRaceScorers");
+    const initData = {
+      raceId: "INIT12345",
+      raceName: "Usain Bolt vs Barry Allen",
+      raceParticipants: [
+        { lane: 1, participantName: "Usain Bolt" },
+        { lane: 2, participantName: "Barry Allen" },
+      ],
+      status: "live",
+      createdAt: new Date(),
+    };
+
+    if (!storedData) {
+      localStorage.setItem("liveHeatsRaceScorers", JSON.stringify([initData]));
+      setRaces([...races, initData]);
+    }
+  }, []);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("liveHeatsRaceScorers");
     let liveHeatsRaceScorerRaces: Race[] = [];
 
     try {
