4602faa2838c47fc77c8618fdacd3a3f41ddf5fb
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2025-02-10 23:31:02 +1300

    refactor: remove redundant fragment

--- src/features/RaceGallery/Components/RaceCreateForm/RaceCreateForm.tsx
+++ src/features/RaceGallery/Components/RaceCreateForm/RaceCreateForm.tsx
@@ -25,44 +25,40 @@ export const RaceCreateForm: FC<RaceCreateFormProps> = ({
   } = useRaceCreateForm(closeFormModal, setRaces);
 
   return (
-    <>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(handleCreateRace)}>
-          <FormLabel>Race name</FormLabel>
-          <ControlledInput
-            control={form.control}
-            name={"raceName"}
-            placeholder={"e.g: Highschool students vs Usain Bolt"}
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleCreateRace)}>
+        <FormLabel>Race name</FormLabel>
+        <ControlledInput
+          control={form.control}
+          name={"raceName"}
+          placeholder={"e.g: Highschool students vs Usain Bolt"}
+        />
+
+        <Separator className={"my-6"} />
+
+        <div
+          className={"flex flex-row items-center text-xs text-muted-foreground"}
+        >
+          <Info size={14} className={"mr-1"} />
+          Press Enter to add a new participant!
+        </div>
+        {fields.map((field, i) => (
+          <AddParticipantField
+            form={form}
+            append={append}
+            remove={remove}
+            key={field.id}
+            index={i}
           />
+        ))}
 
-          <Separator className={"my-6"} />
-
-          <div
-            className={
-              "flex flex-row items-center text-xs text-muted-foreground"
-            }
-          >
-            <Info size={14} className={"mr-1"} />
-            Press Enter to add a new participant!
-          </div>
-          {fields.map((field, i) => (
-            <AddParticipantField
-              form={form}
-              append={append}
-              remove={remove}
-              key={field.id}
-              index={i}
-            />
-          ))}
-
-          <Button
-            disabled={createRaceDisabled}
-            className={"w-full mt-2 font-bold font-archivo"}
-          >
-            Start race!
-          </Button>
-        </form>
-      </Form>
-    </>
+        <Button
+          disabled={createRaceDisabled}
+          className={"w-full mt-2 font-bold font-archivo"}
+        >
+          Start race!
+        </Button>
+      </form>
+    </Form>
   );
 };
